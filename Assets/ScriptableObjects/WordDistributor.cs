c82a019263d55560d8b5bf03ed80395a2b5c0b14
nicolesorense <151392176+nicolesorense@users.noreply.github.com>
2024-10-11 20:14:50 -0400

    Added new words to minigame

--- Assets/ScriptableObjects/WordDistributor.cs
+++ Assets/ScriptableObjects/WordDistributor.cs
@@ -8,12 +8,11 @@ public class WordDistributor : MonoBehaviour
 
     void Awake()
     {
-        stringSets[0].strings = new string[] {"UG", "UC"};
-        stringSets[1].strings = new string[] {"122", "CSD", "CMU", "ABP", "SCS", "CFA", "MCS"};
-        stringSets[2].strings = new string[] {"WEAN", "BOSS", "MSSM", "CIAO", "ROHR", "HUNT"};
-        stringSets[3].strings = new string[] {"CYERT", "SCOTT", "SMITH", "CLYDE", "MUDGE", "WELCH", "ALUMS", "BELLA", "DEFER", "HUNAN", "ANSYS"};
-        stringSets[4].strings = new string[] {"ALUMNI", "BRAMER", "PORTER", "POSNER", "WARNER", "DONNER", "MCGILL", "RESNIK", "STEVER", "MACKEY", "15-112", "15-122", "15-359", "15-151", "15-659", "TUTORS", "15-418", "MELLON", "KOSBIE", "21-127", "SCHATZ"};
-        stringSets[5].strings = new string[] {"DOHERTY", "THE HUB", "SCOBELL", "FREW ST", "TECH ST", "HARDEST", "PURNELL", "LAPRIMA", "ENTROPY", "REDHAWK", "STACKD"};
-        stringSets[6].strings = new string[] {"CARNEGIE", "ART PARK", "MOREWOOD", "CLYDE ST", "HENRY ST", "SCIENCES", "MATRICES", "CALCULUS", "TOP-TIER", "CARNEGIE", "BOOTCAMP", "SUCCEEDS", "RIGOROUS", "MILLIES", "SCOTTYS", "WILDBLUE"};
+        stringSets[0].strings = new string[] {"UG", "UC", "TP"};
+        stringSets[1].strings = new string[] {'SCS', 'CSD', 'PHP', 'MCS', '122', 'CMU', 'SFS', 'CFA', 'ABP'};
+        stringSets[2].strings = new string[] {'ROHR', 'CIAO', 'C0VM', 'BOMB', 'MSSM', 'HUNT', 'WEAN', 'CLAC', 'TBUF', 'BOSS'};
+        stringSets[3].strings = new string[] {'ANSYS', 'MUDGE', 'SMITH', 'DEFER', 'ALUMS', 'BELLA', 'HUNAN', 'CLYDE', 'SCOTT', 'CYERT', 'WELCH'}
+        stringSets[5].strings = new string[] {'BRAMER', 'POSNER', 'MCGILL', 'STACKD', '15-151', 'PORTER', 'TUTORS', '21-127', '15-418', 'SCHATZ', '15-112', 'STEVER', 'RESNIK', '15-122', '15-659', 'MELLON', 'MACKEY', 'DONNER', 'KOSBIE', '15-359', 'WARNER', 'ALUMNI'};
+        stringSets[6].strings = new string[] {'MILLIES', 'REDHAWK', 'SCOTTYS', 'THE HUB', 'ENTROPY', 'LAPRIMA', 'TECH ST', 'DOHERTY', 'HARDEST', 'FREW ST', 'SCOBELL', 'PURNELL'};
     }
 }

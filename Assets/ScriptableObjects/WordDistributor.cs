4c1f0ad0113c61f051b65da9c08328b972d96655
nicolesorense <151392176+nicolesorense@users.noreply.github.com>
2024-10-11 20:17:22 -0400

    Added semicolon;

--- Assets/ScriptableObjects/WordDistributor.cs
+++ Assets/ScriptableObjects/WordDistributor.cs
@@ -11,7 +11,7 @@ public class WordDistributor : MonoBehaviour
         stringSets[0].strings = new string[] {"UG", "UC", "TP"};
         stringSets[1].strings = new string[] {'SCS', 'CSD', 'PHP', 'MCS', '122', 'CMU', 'SFS', 'CFA', 'ABP'};
         stringSets[2].strings = new string[] {'ROHR', 'CIAO', 'C0VM', 'BOMB', 'MSSM', 'HUNT', 'WEAN', 'CLAC', 'TBUF', 'BOSS'};
-        stringSets[3].strings = new string[] {'ANSYS', 'MUDGE', 'SMITH', 'DEFER', 'ALUMS', 'BELLA', 'HUNAN', 'CLYDE', 'SCOTT', 'CYERT', 'WELCH'}
+        stringSets[3].strings = new string[] {'ANSYS', 'MUDGE', 'SMITH', 'DEFER', 'ALUMS', 'BELLA', 'HUNAN', 'CLYDE', 'SCOTT', 'CYERT', 'WELCH'};
         stringSets[5].strings = new string[] {'BRAMER', 'POSNER', 'MCGILL', 'STACKD', '15-151', 'PORTER', 'TUTORS', '21-127', '15-418', 'SCHATZ', '15-112', 'STEVER', 'RESNIK', '15-122', '15-659', 'MELLON', 'MACKEY', 'DONNER', 'KOSBIE', '15-359', 'WARNER', 'ALUMNI'};
         stringSets[6].strings = new string[] {'MILLIES', 'REDHAWK', 'SCOTTYS', 'THE HUB', 'ENTROPY', 'LAPRIMA', 'TECH ST', 'DOHERTY', 'HARDEST', 'FREW ST', 'SCOBELL', 'PURNELL'};
     }
